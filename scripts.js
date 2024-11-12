e42f7ba87eec1de2ab1f6821b1cadb122aa0befc
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2024-11-12 18:37:15 +0300

    adaptive mobile + robot.txt

---
+++ scripts.js
@@ -0,0 +1,47 @@
+// Получаем элементы слайдера
+const slider = document.querySelector(".slider-container__slider");
+const prevButton = document.querySelector(".slider-container__button-left");
+const nextButton = document.querySelector(".slider-container__button-right");
+
+const slides = Array.from(slider.querySelectorAll("img"));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
+
+
+
+
+
+
+
+
+
